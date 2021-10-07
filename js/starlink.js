04e5bcd8911e1d7d865d92a1cbfd7a9171e3add4
Pocket Arcade MX <87832499+PocketArcade@users.noreply.github.com>
2021-10-07 12:00:13 -0500

    Update starlink.js

--- js/starlink.js
+++ js/starlink.js
@@ -12,13 +12,14 @@ $(function(){
 	// Load Audio
 	var audio = new Audio("/sounds/star.mp3");
 	audio.value=11.1;
-	audio._startTime=0;
+	audio._startTime = 0;
 	audio.currentTime = 0;  
 
 	$(popPage).on('click',function(event){
 		audio.play();
 		newWindow = window.open("#","window","width= "+ w + ",height=" + h + ",left=" + l + ",top=" + t + ", scrollbars = yes, location = no, toolbar = no, menubar = no, status = no");
-		if (audio.currentTime - audio._startTime >= audio.value){    
+		if (audio.currentTime - audio._startTime >= audio.value){
+			audio._startTime = 0;
 			audio.currentTime = 0;
 	   } 
 		return false;
